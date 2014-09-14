3d39f6f202edab6459e35af77bafa7a669fc8219
Marius <marius.grad@mindmagnetsoftware.com>
2014-09-14 14:56:39 +0300

    add index action

--- app/code/community/Hackathon/CatalogAttributes/controllers/Adminhtml/Catalog/Category/AttributeController.php
+++ app/code/community/Hackathon/CatalogAttributes/controllers/Adminhtml/Catalog/Category/AttributeController.php
@@ -6,13 +6,39 @@
 class Hackathon_CatalogAttributes_Adminhtml_Catalog_Category_AttributeController 
     extends Mage_Adminhtml_Controller_Action
 {
+    protected $_entityTypeId;
+
+    public function preDispatch()
+    {
+        parent::preDispatch();
+        $this->_entityTypeId = Mage::getModel('eav/entity')->setType(Mage_Catalog_Model_Category::ENTITY)->getTypeId();
+    }
+
+    protected function _initAction()
+    {
+        $this->_title($this->__('Catalog'))
+             ->_title($this->__('Attributes'))
+             ->_title($this->__('Manage Category Attributes'));
+
+        $this->loadLayout()
+            ->_setActiveMenu('catalog/categories_attributes')
+            ->_addBreadcrumb(Mage::helper('catalogattribute')->__('Catalog'), 
+                    Mage::helper('catalogattribute')->__('Catalog'))
+            ->_addBreadcrumb(
+                Mage::helper('catalogattribute')->__('Manage Catalog Attributes'),
+                Mage::helper('catalogattribute')->__('Manage Catalog Attributes'))
+        ;
+        return $this;
+    }
+
     /**
      * Index Action
      */
     public function indexAction()
     {
-        $this->loadLayout();
-        $this->renderLayout();
+        $this->_initAction()
+            ->_addContent($this->getLayout()->createBlock('catalogattribute/adminhtml_catalog_category_attribute'))
+            ->renderLayout();
     }
     
 }
